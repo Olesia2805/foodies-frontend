0393d2bd07647ec468c21131a106ed04b88964fe
vl-vorobei <vorobei.dev@gmail.com>
2025-05-02 21:46:56 +0300

    Added base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { lazy, Suspense } from 'react'
-import { Routes, Route } from 'react-router-dom'
-const HomePage = lazy(() => import('pages/HomePage/HomePage'))
-const UserPage = lazy(() => import('pages/UserPage/UserPage'))
-const RecipePage = lazy(() => import('pages/RecipePage/RecipePage'))
-const AddRecipePage = lazy(() => import('pages/AddRecipePage/AddRecipePage'))
-const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'))
-import SharedLayoutExample from 'components/SharedLayoutExample/SharedLayoutExample'
-import Loader from 'components/Loader/Loader'
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Loader from 'components/Loader/Loader';
+import Layout from './components/Layout/Layout.jsx';
+
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const UserPage = lazy(() => import('pages/UserPage/UserPage'));
+const RecipePage = lazy(() => import('pages/RecipePage/RecipePage'));
+const AddRecipePage = lazy(() => import('pages/AddRecipePage/AddRecipePage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
 
 const App = () => {
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
-        <Route path='/' element={<SharedLayoutExample />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path='/user/:id' element={<UserPage />} />
-          <Route path='/recipe/:id' element={<RecipePage />} />
-          <Route path='/recipe/add' element={<AddRecipePage />} />
-          <Route path='*' element={<NotFoundPage />} />
+          <Route path="/user/:id" element={<UserPage />} />
+          <Route path="/recipe/:id" element={<RecipePage />} />
+          <Route path="/recipe/add" element={<AddRecipePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </Suspense>
-  )
-}
+  );
+};
 
-export default App
+export default App;
