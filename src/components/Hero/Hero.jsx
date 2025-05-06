b3d0dec657ce526de86d0d5b119cabe03c2fd6ce
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2025-05-06 13:23:40 +0300

    refactor Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,12 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import { ROUTER } from '../../constants/router';
 import styles from './Hero.module.css';
 import Container from '../Container/Container';
-import HeroModal from '/src/components/HeroModal/HeroModal';
 import SignInModal from '../SignInModal/SignInModal';
 import { useAuth } from '../../hooks';
+import Button from '../Button/Button';
 
 const Hero = () => {
-  const [showModal, setShowModal] = useState(false);
+  const [isSignInModalOpen, setIsSignInModalOpen] = useState(false);
   const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
@@ -16,16 +16,17 @@ const Hero = () => {
     if (isAuthenticated) {
       navigate(ROUTER.ADD_RECIPE);
     } else {
-      setShowModal(true);
+      setIsSignInModalOpen(true);
     }
-  };
-
-  const closeModal = () => {
-    setShowModal(false);
   };
 
   return (
     <section className={styles.hero}>
+      <div className={`${styles.verticalLine} ${styles.line1}`}></div>
+      <div className={`${styles.verticalLine} ${styles.line2}`}></div>
+      <div className={`${styles.verticalLine} ${styles.line3}`}></div>
+      <div className={`${styles.verticalLine} ${styles.line4}`}></div>
+
       <Container>
         <div className={styles.content}>
           <h1 className={styles.title}>IMPROVE YOUR CULINARY TALENTS</h1>
@@ -33,28 +34,22 @@ const Hero = () => {
             Amazing recipes for beginners in the world of cooking, enveloping
             you in the aromas and tastes of various cuisines.
           </p>
-          <button
-            className={styles.addRecipeBtn}
-            onClick={handleAddRecipeClick}
-          >
-            ADD RECIPE
-          </button>
+          <Button onClick={handleAddRecipeClick}>ADD RECIPE</Button>
         </div>
         <div className={styles.imageGallery}>
           <div className={styles.mainImage}>
             <img src="src/assets/img/1.png" alt="Delicious food" />
           </div>
-          <div className={styles.smallImage}>
+          <div className={styles.secondImage}>
             <img src="src/assets/img/2.png" alt="Tasty dish" />
           </div>
         </div>
       </Container>
 
-      {showModal && (
-        <HeroModal onClose={closeModal}>
-          <SignInModal />
-        </HeroModal>
-      )}
+      <SignInModal
+        isOpen={isSignInModalOpen}
+        onClose={() => setIsSignInModalOpen(false)}
+      />
     </section>
   );
 };
