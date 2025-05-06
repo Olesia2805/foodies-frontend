b2317e4dc353ed2593d4d6a07e371c5017811e59
BalakaMd <md.balaka@gmail.com>
2025-05-06 19:28:37 +0300

    Add customizable Loader component with size variants and fullscreen mode

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,25 @@
-const Loader = () => {
-  return <div>Loading....</div>;
+import React from 'react';
+import clsx from 'clsx';
+import styles from './Loader.module.css';
+
+const Loader = ({ size = 'medium', className, fullScreen = false }) => {
+  const sizeClassMap = {
+    small: styles.small,
+    medium: styles.medium,
+    large: styles.large
+  };
+
+  if (fullScreen) {
+    return (
+      <div className={styles.container} style={{ height: '100vh' }}>
+        <div className={clsx(styles.loader, sizeClassMap[size], className)} />
+      </div>
+    );
+  }
+
+  return (
+    <div className={clsx(styles.loader, sizeClassMap[size], className)} />
+  );
 };
 
 export default Loader;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,20 @@
+import Loader from "components/Loader/Loader";
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      gap: '70px',
+      padding: '50px 0'
+    }}>
+      <Loader size="large" />
+      <Loader size="medium" />
+      <Loader size="small" />
+      {/* <Loader size="large" fullScreen /> */}
+    </div>
+  );
 };
 
 export default HomePage;
