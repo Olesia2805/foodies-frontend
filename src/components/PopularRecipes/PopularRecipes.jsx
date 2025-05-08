0fe95cdcfbe20f763d1a9707e630232aad665a6e
vl-vorobei <vorobei.dev@gmail.com>
2025-05-08 18:53:33 +0300

    Added typography

--- src/components/PopularRecipes/PopularRecipes.jsx
+++ src/components/PopularRecipes/PopularRecipes.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import RecipeCard from '../RecipeCard/RecipeCard.jsx';
 import Container from '../Container/Container.jsx';
 import Loader from '../Loader/Loader.jsx';
+import Typography from '../Typography/Typography.jsx';
 
 import useRecipe from '../../hooks/useRecipe.js';
 
@@ -21,6 +22,8 @@ const PopularRecipes = () => {
   return (
     <div className={styles.popularProducts}>
       <Container>
+        <Typography variant="h3">Popular recipes</Typography>
+
         <div className={styles.popularList}>
           {popularRecipes.map((recipe) => (
             <RecipeCard size="large" key={recipe._id} recipe={recipe} />
