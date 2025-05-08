1449717b9cd56dbb2912987c77a4a003553f622c
Artur Prepelytsia <preart81@gmail.com>
2025-05-08 23:23:26 +0300

    add not found error message

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -34,18 +34,25 @@ const RecipePage = () => {
     loadRecipe();
   }, [id, dispatch]);
 
-  if (loading) return <Loader />;
+  if (loading)
+    return (
+      <Container>
+        <Loader fullScreen={true} />
+      </Container>
+    );
   if (error) return <Error message={error} />;
-  if (!recipe || !recipe._id) return <div>Recipe not found</div>;
+  if (!recipe || !recipe._id)
+    return (
+      <Container>
+        <Error message="Recipe not found" />
+      </Container>
+    );
 
   return (
     <Container>
       {/* Breadcrumbs */}
       <Breadcrumbs
-        items={[
-          { label: 'Home', link: '/' },
-          { label: recipe.title }, 
-        ]}
+        items={[{ label: 'Home', link: '/' }, { label: recipe.title }]}
       />
       <section className={styles.recipeSection}>
         <RecipeImage src={recipe.thumb} alt={recipe.title}></RecipeImage>
