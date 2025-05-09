1ec5bb14c94aadc83baef9a5fd6ef92dff6ae861
akolvakh <a.kolvakh@outlook.com>
2025-05-09 17:19:18 +0300

    #comment ANTON add delete button isCurrentUser

--- src/components/ListRecipeCard/ListRecipeCard.jsx
+++ src/components/ListRecipeCard/ListRecipeCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Icon from '../Icon/Icon';
 import styles from './ListRecipeCard.module.css';
 
-const RecipeCard = ({ recipe }) => {
+const RecipeCard = ({ recipe, isCurrentUser }) => {
   return (
     <div className={styles.recipeCard}>
       <img src={recipe.thumb} alt={recipe.title} className={styles.recipeImage} />
@@ -10,20 +10,28 @@ const RecipeCard = ({ recipe }) => {
         <h3 className={styles.recipeTitle}>{recipe.title}</h3>
         <p className={styles.recipeDescription}>{recipe.description}</p>
       </div>
+      <button
+        className={styles.arrowButton}
+        onClick={() => window.location.href = `/recipes/${recipe._id}`}
+      >
+        <Icon
+          name="arrow-up-right"
+          className={styles.arrowIcon}
+          size={18}
+        />
+      </button>
+
+      {isCurrentUser && (
+      <button className={styles.deleteButton}>
+        <Icon
+          name="trash"
+          className={styles.deleteIcon}
+          size={18}
+        ></Icon>
+      </button>
+      )}
       
-        <button
-          className={styles.arrowButton}
-          onClick={() => window.location.href = `/recipes/${recipe._id}`}
-        >
-                <Icon
-                  name="arrow-up-right"
-                  className={styles.arrowIcon}
-                  size={18}
-                />
-        </button>
-      
-      
-      
+
     </div>
   );
 };
